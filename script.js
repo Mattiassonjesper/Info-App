34910e400876d7394463c647fd4db53ef5096579
Mattiassonjesper <57625574+Mattiassonjesper@users.noreply.github.com>
2019-11-18 15:46:17 +0100

    update
    Co-Authored-By: Marcel Bascour <migelito93@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,53 +1,58 @@
-$( function() {
-    var availableTags = [
-      "Stockholm",
-      "Copenhagen",
-      "Berlin",
-      "Helsinki",
-      "Oslo"
-    ];
-    $( "#city" ).autocomplete({
-      source: availableTags
-    });
-  } );
-  
-  $(document).ready(function(){
-        
-    
-  
-        $("#getWeatherForcast").click(function(){
-                  
-              var city = $("#city").val();
-              var key  = '4de3768c62b67fe359758977a3efc069';
-              
-              $.ajax({
-                url:'http://api.openweathermap.org/data/2.5/weather',
-                dataType:'json',
-                type:'GET',
-                data:{q:city, appid: key, units: 'metric'},
-  
-                success: function(data){
-                  var wf = '';
-                  $.each(data.weather, function(index, val){
-                  
-  
-                    wf += '<p><b>' + data.name + ' ' +  parseInt(data.main.temp) + '&deg;C ' + 
-                    ' | ' + val.main + ", " + val.description 
-  
-                  });
+/*$( function() {
+  var availableTags = [
+    "ActionScript",
+    "AppleScript",
+    "Asp",
+    "BASIC",
+    "C"
+  ];
+  $( "#city" ).autocomplete({
+    source: availableTags
+  });
+} );
+*/
+
+$(document).ready(function(){
+      
+      var callback = function(){
                 
-                 $(".ShowWeatherForcast").html(wf);
-                },
-                error: function (error) {
-                 
-                 var wf = '';
-  
-                   wf += '<p><b>' + error.responseJSON.message + "</b></p>"; 
-  
-                $(".ShowWeatherForcast").html(wf);
-                }
-  
-              })
-  
-        });
+            var city = $("#city").val();
+            var key  = '5cecfc36afc5a098f339672791ce3272';
+            
+            $.ajax({
+              url:'http://api.openweathermap.org/data/2.5/weather',
+              dataType:'json',
+              type:'GET',
+              data:{q:city, appid: key, units: 'metric'},
+
+              success: function(data){
+                var wf = '';
+                $.each(data.weather, function(index, val){
+                
+
+                  wf += '<p><b>' + data.name + ' ' +  parseInt(data.main.temp) + '&deg;C ' + 
+                  ' | ' + val.main + ", " + val.description 
+
+                });
+              
+               $(".ShowWeatherForcast").html(wf);
+              },
+              error: function (error) {
+               
+               var wf = '';
+
+                 wf += '<p><b>' + error.responseJSON.message + "</b></p>"; 
+
+              $(".ShowWeatherForcast").html(wf);
+              }
+
+            })
+      };
+      // Allows the user to call the weather function with the Enter key or the press of the button
+      $("#city").keypress(function() {
+        if (event.which == 13) callback();
       });
+
+      $("#getWeatherForcast").click(callback);
+
+    });
